58be33cc8bf3b7186e753154da8f693aee11529c
Autumn Raine <autumn@inerdgirl.com>
2019-06-21 02:28:58 -0600

    forgot to add index.js 😛

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const ffmpg = require('fluent-ffmpeg');
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('videoSubmit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        console.log('file length is: ', metadata.format.duration)
+    });
 });
